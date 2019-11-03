f5f35c51bac67b5bd7a0ecb0dff59da544b2725b
Phil <thrawn@callandor.com>
2019-11-03 19:33:03 +0100

    add experimental files. initial commit

---
+++ experiment.m
@@ -0,0 +1,35 @@
+% main experiment version 1.0
+
+% initialize settings
+settings
+
+% initialize data structure
+fprintf('starting experiment.\n')
+data.vp = input('please enter vp number: ')
+data.response = nan(settings.repetitions, 1)
+
+% load stimulus
+original_stimulus = imread(settings.stimulus)
+
+% main loop
+for current_run = 1:settings.repetitions
+
+	fprintf('running trial number %i.\n', current_run)
+	
+	% generate comparison stimulus
+	comparison_stimulus = set_brightness(original_stimulus, settings.brightness(current_run))
+	
+	% show experiment screen
+	display_direct(original_stimulus, comparison_stimulus)
+	data.response(current_run) = get_response('which stimulus is brighter (1/2) ?')
+	
+	% wait for ITI
+	pause(settings.speed)
+
+end
+
+% save the resulting data
+data.filename = sprintf('results_vp%03i.mat', data.vp)
+save(data.filename, data, settings)
+
+fprintf('experiment ended.\n')
